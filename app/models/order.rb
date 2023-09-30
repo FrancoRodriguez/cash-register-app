84ecd03df8b222c016b87c59f4c1e6ff075013cf
Franco Rodriguez <rodriguezcfranco@gmail.com>
2023-09-30 17:25:30 +0200

    Disable button to add product into cart if this product already exist

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
   has_many :order_products, dependent: :destroy
+  has_many :products, through: :order_products
+
+  def product_already_in_cart?(product_id)
+    order_products.exists?(product_id:)
+  end
 end
